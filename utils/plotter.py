f786a9a560bd5b335dff5dd127c13a83920f1c9d
Erling Olweus <erlingolweus@gmail.com>
2021-04-25 00:45:45 +0200

    fix plotter

--- utils/plotter.py
+++ utils/plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import torch
-from train import batch_to_img, predb_to_mask
+
+from augmentation import Augmenter
 
 
 def plot_train_and_val_loss(train_loss, valid_loss):
@@ -27,8 +28,24 @@ def predict_on_batch_and_plot(dataset, unet):
     plot_predicted_segmentations(bs, predicted_batch, image_batch, mask_batch)
 
 def plot_image_and_mask(data, idx):
+    aug = Augmenter()
+
     fig, ax = plt.subplots(1,2)
     ax[0].imshow(data.open_as_array(idx))
     ax[1].imshow(data.open_mask(idx))
+    # fig1, fig2 = data.__getitem__(idx)
+    # fig1 = data.open_as_array(idx)
+    # fig2 = data.open_mask(idx)
+    # fig1, fig2 = aug.rotate_image90(image=fig1, mask=fig2)
+    # ax[0].imshow(fig1)
+    # ax[1].imshow(fig2)
     plt.show()
-    
+
+# Support functions
+def batch_to_img(image_batch, idx):
+    img = np.array(image_batch[idx,0:3])
+    return img.transpose((1,2,0))
+
+def predb_to_mask(prediction_batch, idx):
+    p = torch.functional.F.softmax(prediction_batch[idx], 0)
+    return p.argmax(0).cpu()
