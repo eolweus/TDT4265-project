cde92365655026547ad4479c163600b2aefbf328
Erling Olweus <erlingolweus@gmail.com>
2021-04-25 16:42:30 +0200

    Fix albumentations (#9)
    * add function for resizing tee images
    
    * make augmentation and tee masks work
    
    * add support for yaml files

--- utils/plotter.py
+++ utils/plotter.py
@@ -28,17 +28,18 @@ def predict_on_batch_and_plot(dataset, unet):
     plot_predicted_segmentations(bs, predicted_batch, image_batch, mask_batch)
 
 def plot_image_and_mask(data, idx):
-    aug = Augmenter()
-
     fig, ax = plt.subplots(1,2)
     ax[0].imshow(data.open_as_array(idx))
     ax[1].imshow(data.open_mask(idx))
-    # fig1, fig2 = data.__getitem__(idx)
-    # fig1 = data.open_as_array(idx)
-    # fig2 = data.open_mask(idx)
-    # fig1, fig2 = aug.rotate_image90(image=fig1, mask=fig2)
-    # ax[0].imshow(fig1)
-    # ax[1].imshow(fig2)
+    plt.show()
+
+def plot_with_augmentations(data, idx):
+    aug = Augmenter()
+
+    fig, ax = plt.subplots(1,2)
+    fig1, fig2 = data.remove_image_borders(idx)
+    ax[0].imshow(fig1)
+    ax[1].imshow(fig2)
     plt.show()
 
 # Support functions
